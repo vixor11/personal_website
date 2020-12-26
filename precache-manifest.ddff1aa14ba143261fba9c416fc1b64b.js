f061e2d7c46b77302829be8cf083f5a3d3ee2507
vixor11 <victorcc@stanford.edu>
2020-12-25 23:37:07 -0400

    Updates

---
+++ precache-manifest.ddff1aa14ba143261fba9c416fc1b64b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17aef6d8cc73a5a854aaf59ebbcc1162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46d1c065db09a0199c7b",
+    "url": "/static/css/2.11c8fdcf.chunk.css"
+  },
+  {
+    "revision": "7d795392c98717c0d45f",
+    "url": "/static/css/main.5c9d7fad.chunk.css"
+  },
+  {
+    "revision": "46d1c065db09a0199c7b",
+    "url": "/static/js/2.17d45a50.chunk.js"
+  },
+  {
+    "revision": "b69182ba16e052c90e745c80b1257de8",
+    "url": "/static/js/2.17d45a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d795392c98717c0d45f",
+    "url": "/static/js/main.9c71ebf0.chunk.js"
+  },
+  {
+    "revision": "cb5e4625273608814864",
+    "url": "/static/js/runtime-main.4d26892b.js"
+  },
+  {
+    "revision": "3044ea6114f7bac3a362a20770ed92cf",
+    "url": "/static/media/edav.3044ea61.jpg"
+  },
+  {
+    "revision": "80538961415a8a3e2d53162ffff15864",
+    "url": "/static/media/genericSocialMedia.80538961.jpg"
+  },
+  {
+    "revision": "b7ee32aa73d44ffd88a4e29b4d7ae2ed",
+    "url": "/static/media/headshot2019 copy.b7ee32aa.JPG"
+  },
+  {
+    "revision": "6768f224cc80627c55a63bcdbd6256a7",
+    "url": "/static/media/library.6768f224.jpg"
+  },
+  {
+    "revision": "088210afcc88fd4841bb1d2d1286902c",
+    "url": "/static/media/photoShare.088210af.jpg"
+  },
+  {
+    "revision": "f32bf678377a6cfd023525b6a9fe3796",
+    "url": "/static/media/resume.f32bf678.pdf"
+  },
+  {
+    "revision": "11fb95b855fb0fe54472b314929e5a8c",
+    "url": "/static/media/startupIdeas.11fb95b8.pdf"
+  },
+  {
+    "revision": "892beae37418a824a7261fc2c0daeca2",
+    "url": "/static/media/tabTab.892beae3.jpg"
+  }
+]);
